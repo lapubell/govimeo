c009a1ed6be8565f9c574fd2a89643e4487e25bf
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2018-02-15 10:00:53 -0800

    can get correct duration from fake test data

--- govimeo.go
+++ govimeo.go
@@ -1,16 +1,79 @@
 package govimeo
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 )
 
 var apiToken string
+var video vimeoVideo
 
+type vimeoVideo struct {
+	URI      string `json:"uri"`
+	Name     string `json:"name"`
+	Duration int    `json:"duration"`
+	Pictures struct {
+		URI string `json:"uri"`
+	} `json:"pictures"`
+}
+
+// SetToken allows you to pass your API token from a different package or the
+// main function
 func SetToken(s string) error {
 	if s == "" {
 		return errors.New("token can not be empty")
 	}
 
 	apiToken = s
+	return nil
+}
+
+// GetVideoDuration will return the duration of the video by making a GET
+// request to the Vimeo API
+func GetVideoDuration(vID uint) (int, error) {
+	if len(apiToken) == 0 {
+		return 0, errors.New("Please set your token")
+	}
+
+	// check and see if we need to request new data
+	if "/videos/"+strconv.Itoa(int(vID)) != video.URI {
+		err := requestDataFromVimeo(vID)
+		if err != nil {
+			return 0, errors.New("Problem getting data from vimeo")
+		}
+		return video.Duration, nil
+	}
+
+	return video.Duration, nil
+}
+
+func requestDataFromVimeo(vID uint) error {
+	v := vimeoVideo{}
+	// fake data so i'm not doing a real http request during testing
+	if apiToken == "testing" {
+		v.Duration = 100
+		v.Name = "fake video"
+		v.Pictures.URI = "/videos/" + strconv.Itoa(int(vID)) + "/pictures/654321"
+		v.URI = "/videos/" + strconv.Itoa(int(vID)) + ""
+
+		video = v
+		return nil
+	}
+
+	response, err := http.Get("https://api.vimeo.com/videos/" + strconv.Itoa(int(vID)))
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	err = json.Unmarshal(body, &v)
+	if err != nil {
+		return errors.New("problem with the response from vimeo")
+	}
+
+	video = v
 	return nil
 }
--- govimeo_test.go
+++ govimeo_test.go
@@ -22,3 +22,23 @@ func TestSetToken(t *testing.T) {
 		t.Errorf("the token didn't get set correctly")
 	}
 }
+
+func TestGetVideoDuration(t *testing.T) {
+	// reset the token to a zero state
+	apiToken = ""
+	_, e := GetVideoDuration(123)
+	if e.Error() != "Please set your token" {
+		t.Errorf("Token is required to make a call")
+	}
+
+	apiToken = "testing"
+	d, e := GetVideoDuration(123)
+	if d != 100 {
+		t.Errorf("expected 100 from the fake data")
+	}
+	// call it with the hardcoded fake data make sure that it doesn't request data from vimeo unless
+	d, e = GetVideoDuration(123)
+	if d != 100 {
+		t.Errorf("expected 100 from the fake data")
+	}
+}
